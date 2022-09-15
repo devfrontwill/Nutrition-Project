2b7036a5fdb032cb14ad6236177d07a67d069d88
Wiliam Roza <devfrontwill@gmail.com>
2022-09-14 21:10:53 -0300

    Criado um formulario para add pacientes dinamicamente na lista

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
-    var paciente = pacientes[i];
+        var paciente = pacientes[i];
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+        var tdPeso = paciente.querySelector(".info-peso");
+        var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+        var tdAltura = paciente.querySelector(".info-altura");
+        var altura = tdAltura.textContent;
 
-    var tdImc = paciente.querySelector(".info-imc");
+        var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+        var pesoEhValido = true;
+        var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido!";
-        paciente.classList.add("paciente-invalido");
+        if (peso <= 0 || peso >= 1000) {
+            pesoEhValido = false;
+            tdImc.textContent = "Peso inválido!";
+            paciente.classList.add("paciente-invalido");
+        }
+
+        if (altura <= 0 || altura >= 3.00) {
+            alturaEhValida = false;
+            tdImc.textContent = "Altura inválida!";
+            paciente.classList.add("paciente-invalido");
+        }
+
+        if (alturaEhValida && pesoEhValido) {
+            var imc = peso / (altura * altura);
+            tdImc.textContent = imc.toFixed(2);
+        }
     }
 
-    if (altura <= 0 || altura >= 3.00) {
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inválida!";
-        paciente.classList.add("paciente-invalido");
-    }
-
-    if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
-    }
-    }
+    var botaoAdicionar = document.querySelector("#adicionar-paciente");
+    botaoAdicionar.addEventListener("click", function(event){
+        event.preventDefault();
+    })
 
 
 
