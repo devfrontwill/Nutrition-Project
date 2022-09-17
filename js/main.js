8cfdd67244b5cbdb63594e873ad7723a50714688
Wiliam Roza <devfrontwill@gmail.com>
2022-09-17 18:46:46 -0300

    Criado objetos para reduzir o volume do codigo

--- js/main.js
+++ js/main.js
@@ -27,47 +27,19 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (alturaEhValida && pesoEhValido) {
-            var imc = peso / (altura * altura);
-            tdImc.textContent = imc.toFixed(2);
+            var imc = calculaImc(peso,altura);
+            tdImc.textContent = imc;
     }
     }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event){
-    event.preventDefault();
+function calculaImc(peso,altura) {
+        var imc = 0;
 
-    var form = document.querySelector("#form-adiciona");
+        imc = peso / (altura * altura);
 
-    var nome = form.nome.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value;
+        return imc.toFixed(2);
+}
 
-    var pacienteTr = document.createElement("tr");
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr); 
-
-
-});
 
     
 
